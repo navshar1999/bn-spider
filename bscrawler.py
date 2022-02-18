7feefb4fe375ca022dd6b94cdaeabda71c321f55
navshar1999 <naveena.sharma@gmail.com>
2022-02-18 12:37:45 -0600

    extract titles

--- bscrawler.py
+++ bscrawler.py
@@ -1,20 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-header = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.50'}
+header = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'}
 
 def book_spider(max_pages): 
     page = 1
-    print("page",page)
     while page <= max_pages:
         url = 'https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8?Nrpp=20&page=' + str(page)
         source_code = requests.get(url,headers=header)
         plain_text = source_code.content
         soup = BeautifulSoup(plain_text,features="html.parser")
-        for link in soup.findAll('a',{ 'class':''}):
-            href = link.get('href')
-            title = link.string
-            print(title)
-        page += 1 
+        for product_header in soup.findAll('h3',{'class':'product-info-title'}):
+            print(product_header.get_text()) 
+        page = page + 1 
 
-book_spider(3)
+book_spider(1)
+
